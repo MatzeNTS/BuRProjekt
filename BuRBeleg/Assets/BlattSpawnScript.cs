e6dd47804facd14cb1974d1042c3a513d99005f7
MatzeNTS <matthias.neitsch@gmx.de>
2026-01-19 21:02:21 +0100

    Fehler Blattspawn, Rotation bei Spawn
    -Fehler bei Blartspawn gefixt, bei dem die Blätter nur übereinander spawnen
    -Random Rotation bei Blattspawn

--- BuRBeleg/Assets/BlattSpawnScript.cs
+++ BuRBeleg/Assets/BlattSpawnScript.cs
@@ -7,7 +7,7 @@ public class BlattSpawnScript : MonoBehaviour
     public float spawnRate = 2;
     public float timer = 0;
     public float heightOffset = 1;
-    public float widthOffset = 50;
+    public float widthOffset = 10;
     public int spawnAmount = 3;
 
 
@@ -41,15 +41,31 @@ public class BlattSpawnScript : MonoBehaviour
 
     void spawnLeaf(float heightOffset, float widthOffset)
     {
+        Vector3 pos = new Vector3(
+            Random.Range(-8f, -4f),     //Koordinaten breite Baumkrone
+            Random.Range(2.7f, -1.7f),  //Koordinaten Höhe Baumkrone
+            0
+        );
+
+        GameObject leaf = Instantiate(Blatt, pos, Quaternion.Euler(0.0f, 0.0f, Random.Range(0.0f, 360.0f)));
+        Debug.Log("Spawned at: " + pos);
+    }
+
+    /*
+    void spawnLeaf(float heightOffset, float widthOffset)
+    {
         float lowestPoint = transform.position.y - heightOffset; //Position Höhe
         float highestPoint =  transform.position.y + heightOffset;
         float leftestPoint = transform.position.x - widthOffset; //Position Seitlich
         float rightestPoint = transform.position.x + widthOffset;
+        Debug.Log("Y-Koordinate: " + transform.position);
+        Debug.Log("X-Koordinate: " + transform.position);
 
-        GameObject leaf = Instantiate(Blatt, new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0), transform.rotation);
+        GameObject leaf = Instantiate(Blatt, new Vector3(Random.Range(leftestPoint, rightestPoint), Random.Range(lowestPoint, highestPoint), 0), Quaternion.identity, null); //null = kein parent
         leaf.GetComponent<BlattBehaviourScript>().enableBlattGravity(0f);
+        leaf.transform.SetParent(null);
 
         Debug.Log("Spawn: " + leaf.transform.position);
-    }
+    }*/
 
 }
